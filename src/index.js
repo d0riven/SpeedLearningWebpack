d27198007476c1ad981e0a898c0fa814ce18c8e6
d0riven <wega315@gmail.com>
2020-02-08 16:50:23 +0900

    debug log only development

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import { APP_NAME, hello, Figure} from "./myutil";
+
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  console.log('!!開発モードで動作中!!');
+}
 console.log(APP_NAME);
 hello();
 console.log(Figure.getTriangle(10, 5));
